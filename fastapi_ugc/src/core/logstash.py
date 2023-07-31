9aa5847b107e2ef54ce17829e6826fe489677a21
Vladimir Krasnov <nanoboot@yandex.ru>
2023-07-31 15:24:47 +0300

    Task12. Code review fixes

--- fastapi_ugc/src/core/logstash.py
+++ fastapi_ugc/src/core/logstash.py
@@ -4,20 +4,24 @@ from logstash_async.handler import AsynchronousLogstashHandler, LogstashFormatte
 from core.config import base_settings
 
 
-logger = logging.getLogger('')
+logger = logging.getLogger("")
 
 
 def config_logstash():
-    formatter = LogstashFormatter(tags=['events_ugc', ])
+    formatter = LogstashFormatter(
+        tags=[
+            "events_ugc",
+        ]
+    )
     handler = AsynchronousLogstashHandler(
         base_settings.logstash_host,
         base_settings.logstash_port,
-        transport='logstash_async.transport.UdpTransport',
-        database_path='logstash.db',
+        transport="logstash_async.transport.UdpTransport",
+        database_path="logstash.db",
     )
     handler.formatter = formatter
     logger.addHandler(handler)
 
 
 def add_log_request_id(request_id: str | None):
-    logger.info(f'request_id {request_id}')
+    logger.info(f"request_id {request_id}")
